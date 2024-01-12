e5dd4ef3ec92349813301d2c45873ab428f180ef
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-01-12 16:36:57 -0500

    defaults

--- src/pages/og/[...slug].ts
+++ src/pages/og/[...slug].ts
@@ -18,6 +18,7 @@ export const { getStaticPaths, GET } = OGImageRoute({
       bgImage: {
         path: "./src/assets/taiko-og-bg.png",
         position: "start",
+        fit: "fill",
       },
       border: {
         width: 20,
