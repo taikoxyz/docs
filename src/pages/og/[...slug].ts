f8d44740f521d10036e6222847294a0677ed79a2
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-01-12 00:38:04 -0500

    try custom images

--- src/pages/og/[...slug].ts
+++ src/pages/og/[...slug].ts
@@ -27,7 +27,7 @@ export const { getStaticPaths, GET } = OGImageRoute({
         fit: "cover",
         path: "./src/assets/taiko-og.png",
       },
-      padding: 30,
+      padding: 150,
     };
   },
 });
