8f883342710056473d5af85a116f2902d01a863d
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-01-12 11:04:51 -0500

    update astro/starlight and try new logo thing for og image

--- src/pages/og/[...slug].ts
+++ src/pages/og/[...slug].ts
@@ -1,28 +1,23 @@
 import { getCollection } from "astro:content";
 import { OGImageRoute } from "astro-og-canvas";
 
-// Get all entries from the `docs` content collection.
 const entries = await getCollection("docs");
-
-// Map the entry array to an object with the page ID as key and the
-// frontmatter data as value.
 const pages = Object.fromEntries(entries.map(({ data, id }) => [id, { data }]));
 
+// See https://github.com/delucis/astro-og-canvas for the full API
 export const { getStaticPaths, GET } = OGImageRoute({
-  // Pass down the documentation pages.
   pages,
-  // Define the name of the parameter used in the endpoint path, here `slug`
-  // as the file is named `[...slug].ts`.
   param: "slug",
-  // Define a function called for each page to customize the generated image.
   getImageOptions: (_path, page: (typeof pages)[number]) => {
     return {
-      // Use the page title and description as the image title and description.
       title: page.data.title,
       description: page.data.description,
+      logo: {
+        path: "./src/assets/taiko-og-logo.png",
+      },
       bgImage: {
         fit: "cover",
-        path: "./src/assets/taiko-og.png",
+        path: "./src/assets/taiko-og-bg.png",
       },
       padding: 180,
     };
