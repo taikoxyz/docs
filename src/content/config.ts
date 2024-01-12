ca4bf35b9fb35cbbfb1cd0a0092687e212f914d4
d1onys1us <13951458+d1onys1us@users.noreply.github.com>
2024-01-12 18:35:21 -0500

    implement the description thing, TODO: add all other descriptions

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,13 @@
-import { defineCollection } from "astro:content";
+import { z, defineCollection } from "astro:content";
 import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
-  docs: defineCollection({ schema: docsSchema() }),
+  docs: defineCollection({
+    schema: docsSchema({
+      extend: z.object({
+        description: z.string().length(160, "Must be 160 characters or less."),
+      }),
+    }),
+  }),
   i18n: defineCollection({ type: "data", schema: i18nSchema() }),
 };
--- src/content/docs/api-reference/guardian-prover-api.md
+++ src/content/docs/api-reference/guardian-prover-api.md
@@ -1,5 +1,10 @@
 ---
 title: Guardian Prover API
+description: The Guardian Prover API is used to fetch data about the status of Taiko's Guardian Provers.
 ---
+
+:::note
+Work in progress. We don't have the API up for this yet, but it's coming soon. Bother cyberhorsey on Discord if not here yet.
+:::
 
 Guardian Prover API: [https://guardian-prover.katla.taiko.xyz/](https://guardian-prover.katla.taiko.xyz/).
